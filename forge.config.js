679cabaec55f5180ca3392587d5301480378d8f3
Parker Mossman <parkermos@gmail.com>
2025-05-03 23:07:04 -0700

    get dmg publishing working

--- forge.config.js
+++ forge.config.js
@@ -4,24 +4,26 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: 'assets/mirrorbast-' // Base name for icons
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {}, // Windows specific config (including signing) goes here
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: '@electron-forge/maker-dmg',
+      config: {
+        icon: 'assets/mirrorbast-icon.icns', // Specific icon for DMG
+        format: 'ULFO'
+      },
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        // options: { icon: 'assets/mirrorbast-icon.png' } // Optional icon for Debian package
+      },
     },
   ],
   plugins: [
@@ -29,8 +31,6 @@ module.exports = {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
